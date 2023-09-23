bde1087b486270d6870f75660363611fe03661f5
RenierRains <spideypeter04@gmail.com>
2023-09-23 21:41:44 +0800

    loginFix2

--- app/src/main/java/com/example/finalproject/SignUp.kt
+++ app/src/main/java/com/example/finalproject/SignUp.kt
@@ -4,18 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.example.finalproject.SignUpActivity.databinding.ActivitySignUpBinding
+import com.example.finalproject.databinding.ActivitySignUpBinding
 import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity() {
 
-    private lateinit var binding: SignUpActivity
+    private lateinit var binding: ActivitySignUpBinding // Corrected binding type
     private lateinit var firebaseAuth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater) // Corrected binding initialization
         setContentView(binding.root)
 
         firebaseAuth = FirebaseAuth.getInstance()
@@ -24,6 +23,7 @@ class SignUpActivity : AppCompatActivity() {
             val intent = Intent(this, SignInActivity::class.java)
             startActivity(intent)
         }
+
         binding.button.setOnClickListener {
             val email = binding.emailEt.text.toString()
             val pass = binding.passET.text.toString()
@@ -31,14 +31,12 @@ class SignUpActivity : AppCompatActivity() {
 
             if (email.isNotEmpty() && pass.isNotEmpty() && confirmPass.isNotEmpty()) {
                 if (pass == confirmPass) {
-
-                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener {
-                        if (it.isSuccessful) {
+                    firebaseAuth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener { task ->
+                        if (task.isSuccessful) {
                             val intent = Intent(this, SignInActivity::class.java)
                             startActivity(intent)
                         } else {
-                            Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
-
+                            Toast.makeText(this, task.exception?.message, Toast.LENGTH_SHORT).show() // Use task.exception?.message to get the error message
                         }
                     }
                 } else {
@@ -46,7 +44,6 @@ class SignUpActivity : AppCompatActivity() {
                 }
             } else {
                 Toast.makeText(this, "Empty Fields Are not Allowed !!", Toast.LENGTH_SHORT).show()
-
             }
         }
     }
