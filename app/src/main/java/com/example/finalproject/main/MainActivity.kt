63408f6dfe105bb6ddfa2141a8cce2039c0928d0
RenierRains <spideypeter04@gmail.com>
2023-09-26 00:40:46 +0800

    test

--- app/src/main/java/com/example/finalproject/main/MainActivity.kt
+++ app/src/main/java/com/example/finalproject/main/MainActivity.kt
@@ -9,7 +9,27 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        binding.bottomNavigationView.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.home -> {
+                    // Handle home item
+                }
+                R.id.profile -> {
+                    // Handle profile item
+                }
+                R.id.settings -> {
+                    // Handle settings item
+                }
+            }
+            true
+        }
 
+        private fun replaceFragment(fragment: Fragment) {
+            val fragmentManager = supportFragmentManager
+            val fragmentTransaction = fragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.frame_layout, fragment)
+            fragmentTransaction.commit()
+        }
 
     }
 }
